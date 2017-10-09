858477fbef1ed8468189f85d00fbd7de1748cf4d
mnikn <329121948@qq.com>
2017-10-09 22:34:15 +0800

    add strings

---
+++ typescript/strings.ts
@@ -0,0 +1,10 @@
+export class StringUtils {
+    public static getFirstSpaceCount(str: string): number {
+        let i = 0;
+        let firstSpaceCount = 0;
+        while (i < str.length && str[i++] === ' ') {
+            ++firstSpaceCount;
+        }
+        return firstSpaceCount;
+    }
+}
